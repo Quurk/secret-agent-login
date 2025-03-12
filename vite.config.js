fd7733fb367bc03084199bfbff941037ab852cf6
choonhao21 <98331274+namejeb@users.noreply.github.com>
2025-03-12 15:16:54 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/brownchickenbrowncow/',  // Must include this because of how the gh-page directories are organized for the environments (build artifact for this environment will reside in "development" subdirectory)
+  base: '/brownchickenbrowncow/',  // Must include this because of how the gh-page directories are organized for the environments (build artifact for this environment will reside in the subdirectory of the same name)
 })
